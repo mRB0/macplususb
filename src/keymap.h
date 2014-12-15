992873fc780fad6615a335c6ae227396e8ca166f
mRB0 <mrburke@gmail.com>
2014-12-14 21:59:06 -0400

    Add keypad keys and 'shifted' keypad keys (keys that used to be handled on the keypad by holding shift)

--- src/keymap.h
+++ src/keymap.h
@@ -4,5 +4,7 @@
 #include <stdint.h>
 
 extern uint8_t const AppleScancodeToUSBKey[128];
+extern uint8_t const AppleKeypadScancodeToUSBKey[128];
+extern uint8_t const AppleShiftedKeypadScancodeToUSBKey[128];
 
 #endif
